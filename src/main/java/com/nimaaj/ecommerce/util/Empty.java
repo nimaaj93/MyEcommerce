c057bef670cbdd1052b123d6824b022909d98c81
nima.aj93 <nima.aj93@gmail.com>
2019-03-03 00:52:09 +0330

    fix Empty

--- src/main/java/com/nimaaj/ecommerce/util/Empty.java
+++ src/main/java/com/nimaaj/ecommerce/util/Empty.java
@@ -7,19 +7,19 @@ import java.util.Collection;
  */
 public abstract class Empty {
 
-    public boolean isEmpty(String str) {
+    public static boolean isEmpty(String str) {
         return str == null || str.isEmpty();
     }
 
-    public boolean isBlank(String str) {
+    public static boolean isBlank(String str) {
         return str == null || str.isEmpty() || str.trim().isEmpty();
     }
 
-    public boolean isEmpty(Collection collection) {
+    public static boolean isEmpty(Collection collection) {
         return collection == null || collection.isEmpty();
     }
 
-    public boolean isEmpty(Object obj) {
+    public static boolean isEmpty(Object obj) {
         if (obj instanceof String) {
             return isEmpty(isEmpty((String)obj));
         } else if (obj instanceof Collection) {
@@ -29,7 +29,7 @@ public abstract class Empty {
         }
     }
 
-    public boolean isNotEmpty(Object obj) {
+    public static boolean isNotEmpty(Object obj) {
         return !isEmpty(obj);
     }
 
