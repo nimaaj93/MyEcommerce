2767a0bd7b988b0af2d6f27d8b9bcf8ae9f024de
n.ajdari <1qaz!QAZ>
2019-09-29 16:55:51 +0330

    some improvements

---
+++ src/main/java/com/nimaaj/ecommerce/exception/DuplicateProductUserBagException.java
@@ -0,0 +1,8 @@
+package com.nimaaj.ecommerce.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "duplicate.user.bag")
+public class DuplicateProductUserBagException extends BaseException {
+}
--- src/main/java/com/nimaaj/ecommerce/repository/UserBagRepository.java
+++ src/main/java/com/nimaaj/ecommerce/repository/UserBagRepository.java
@@ -4,6 +4,11 @@ import com.nimaaj.ecommerce.domain.UserBag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserBagRepository extends JpaRepository<UserBag, Long> {
+
+    List<UserBag> findAllByUser_Id(Long id);
+
 }
